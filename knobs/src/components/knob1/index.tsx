27c867c7dd30c7cc51f77caad08736aec8a17dd5
AlexRatmansky <alexratmansky@yandex.ru>
2020-05-02 00:41:27 +0300

    add draggability

--- knobs/src/components/knob1/index.tsx
+++ knobs/src/components/knob1/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useReducer } from 'react';
 import style from './knob.module.scss';
 import { Div } from '../../types/common-types';
 
@@ -6,20 +6,61 @@ interface Props {
   label: string;
 }
 
-export const Knob1: FC<Props> = props => {
-  const [rotation, setRotation] = useState(30);
+const initialState = { rotation: 30 };
 
-  const handleMouseMove = (e: any) => {
-    setRotation(rotation + 2);
-    console.log('move', e);
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case 'change':
+      return { rotation: state.rotation + action.data };
+
+    default:
+      throw new Error();
+  }
+}
+
+export const Knob1: FC<Props> = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { rotation } = state;
+
+  const handleMouseClick = (e: any) => {
+    const el = e.target;
+
+    console.log('enter PointerLock');
+    el.requestPointerLock();
+
+    document.addEventListener(
+      'pointerlockchange',
+      () => {
+        if (document.pointerLockElement === el) {
+          console.log('The pointer lock status is now locked');
+          document.addEventListener(
+            'mousemove',
+            (e) => {
+              requestAnimationFrame(() => {
+                console.log(e, e.movementX, e.movementY);
+                console.log(rotation);
+
+                dispatch({ type: 'change', data: e.movementY });
+                // setRotation(rotation + e.movementY);
+              });
+            },
+            false
+          );
+        }
+      },
+      false
+    );
   };
+
+  console.log(rotation);
 
   return (
     <KnobContainer>
       <KnobDial>
-        <KnobGrip rotation={rotation} onMouseMove={handleMouseMove} />
+        <KnobGrip rotation={rotation} />
       </KnobDial>
-      <KnobLabel>{props.label}</KnobLabel>
+      <KnobLabel onMouseDown={handleMouseClick}>{props.label}</KnobLabel>
     </KnobContainer>
   );
 };
@@ -42,7 +83,7 @@ const KnobGrip: FC<KnobGrip & Div> = ({ rotation, children, ...props }) => (
   <div
     className={style.grip}
     style={{
-      transform: `translate(-50%, -50%) rotate(${rotation}deg)`
+      transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
     }}
     {...props}
   >
@@ -50,6 +91,8 @@ const KnobGrip: FC<KnobGrip & Div> = ({ rotation, children, ...props }) => (
   </div>
 );
 
-const KnobLabel: FC<Div> = ({ children }) => (
-  <div className={style.label}>{children}</div>
+const KnobLabel: FC<Div> = ({ children, onMouseDown }) => (
+  <div onMouseDown={onMouseDown} className={style.label}>
+    {children}
+  </div>
 );
